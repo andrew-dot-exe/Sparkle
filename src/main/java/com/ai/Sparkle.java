27ca1b12d87ac73b4aa5416750f419e1ff652db0
andrew-dot-exe <andrejnikitin974@gmail.com>
2024-11-30 16:43:29 +0400

    cleanup&jar compile fix

--- src/main/java/com/ai/Sparkle.java
+++ src/main/java/com/ai/Sparkle.java
@@ -1,22 +1,14 @@
 package com.ai;
 
 import com.ai.dataSet.Data;
-import com.ai.dataSet.DataSet;
-import com.ai.neuralNetwork.train.TrainingNeuralNetwork;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Sparkle {
 
-    private final String workdir;
     private final Ensemble ensemble;
 
     public Sparkle()
     {
-        workdir = System.getProperty("user.dir");
+        String workdir = System.getProperty("user.dir");
         ensemble = new Ensemble(workdir + "/weight/");
         /* todo: check weight directory, if not present, get the train csv and save as train folder */
     }
@@ -28,35 +20,6 @@ public class Sparkle {
         System.out.println(sp.getBurnRate(testData));
     }
 
-    private void displayTestDatasetResult()
-    {
-        try(BufferedReader br = new BufferedReader(new FileReader(workdir  + "/test.csv"))) {
-            Ensemble ensemble = new Ensemble(workdir + "/weight/");
-            String dataLine = br.readLine();
-            while ((dataLine = br.readLine()) != null){
-                ensemble.description(dataLine);
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-    private void train()
-    { //метод для тренировки нейросети
-        String csv_dataset = workdir + "/train.csv";
-        DataSet dataSet = new DataSet(csv_dataset);
-
-        String weightsFile = workdir + "/weight/weight9.csv"; //почему
-
-        double speed = 0.0005;
-        double alpha = 0.08;
-        double maxWeight = 10;
-
-        TrainingNeuralNetwork tnn = new TrainingNeuralNetwork(weightsFile, speed, alpha, maxWeight);
-
-                tnn = new TrainingNeuralNetwork(new int[]{7, 3, 1}, 0.0005, 0.08, 10);
-                tnn.save(weightsFile);
-
-    }
     public String resultExplain(Data data, double result){return null;}
 
     public double getBurnRate(Data data){
